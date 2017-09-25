a3051ee0b53b4b645e7bfb714ee4268524433d29
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-09-25 06:30:38 -0300

    10.4 add forms of Pesquisa e lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { TabViewModule } from 'primeng/primeng';
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import { ButtonModule } from 'primeng/components/button/button';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    TabViewModule
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
