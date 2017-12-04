24e3d9f028a36558a3c336132e170c5d0cbe5c85
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-12-04 06:39:02 -0300

    13.2 add Calendar (+extra)

--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -8,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentoCadastroComponent implements OnInit {
 
   constructor() { }
+    pt_BR: any;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.pt_BR = {
+          firstDayOfWeek: 0,
+          dayNames: [ "Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado" ],
+          dayNamesShort: [ "dom","seg","ter","qua","qui","sex","sáb" ],
+          dayNamesMin: [ "D","S","T","Q","Q","S","S" ],
+          monthNames: [ "Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro" ],
+          monthNamesShort: [ "Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez" ]
+        };
+    }
 
 }
