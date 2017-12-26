ae113db2441ec133bcdfc0d0b70e4a1feaed5fee
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-12-26 11:50:46 -0300

    17.14 desafio: mensagem de erro

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,6 @@
 import { ToastyService } from 'ng2-toasty';
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
 
 @Injectable()
 export class ErrorHandlerService {
@@ -9,11 +10,18 @@ export class ErrorHandlerService {
   handle(error: any) {
     let msg: string;
 
-    if (typeof error === 'string') {
-      msg = error;
+    if (error instanceof Response && error.status >= 400 && error.status <= 499) {
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+      try {
+        errors = error.json();
+        msg = errors[0].userMessage;
+      } catch (e) {
+        console.log('Não foi possível obter o Json');
+      }
     } else {
       msg = 'Erro ao processar serviço remoto.';
-      console.log(error);
+      console.log('', error);
     }
     this.toasty.error(msg);
   }
