2daa9a62aa64945be36ddeaa81b67e789fce69a7
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-12-29 04:08:53 -0300

    18.2 in-app routing setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,21 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+import { LancamentoPesquisaComponent } from './lancamentos/lancamento-pesquisa/lancamento-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoaPesquisaComponent } from './pessoas/pessoa-pesquisa/pessoa-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentoPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'pessoas', component: PessoaPesquisaComponent },
+  { path: 'pessoas/novo', component: PessoaCadastroComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +29,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
+    RouterModule.forRoot(routes),
 
     LancamentosModule,
     PessoasModule,
