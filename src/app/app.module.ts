42d77301a95ce6439cfa4f56b39e0f58d22b9698
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-12-31 10:13:05 -0300

    18.10 fazendo redirecionamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PessoaPesquisaComponent } from './pessoas/pessoa-pesquisa/pessoa-pesqui
 import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
   { path: 'lancamentos', component: LancamentoPesquisaComponent },
   { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
   { path: 'lancamentos/:id', component: LancamentoCadastroComponent },
